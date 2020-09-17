cc6ea4e8cc3c4b67ab37096b47faee64fcf3b8dd
Jacob Carver <55562256+jake10693@users.noreply.github.com>
2020-09-16 17:48:10 -0700

    added game board - still working on creating movement for pac man

--- pacman.js
+++ pacman.js
@@ -1,4 +1,105 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
-    const scoreDisplay = document.getElementById('')
+    const scoreDisplay = document.getElementById('score')
+    const width = 28  //28 * 28 = 784 squares
+
+    //Layout of grid and what is in the squares
+
+    const layout = [
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+        1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+        1,3,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,3,1,
+        1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+        1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+        1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+        1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+        1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+        1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,
+        1,1,1,1,1,1,0,1,1,4,4,4,4,4,4,4,4,4,4,1,1,0,1,1,1,1,1,1,
+        1,1,1,1,1,1,0,1,1,4,1,1,1,2,2,1,1,1,4,1,1,0,1,1,1,1,1,1,
+        1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+        4,4,4,4,4,4,0,0,0,4,1,2,2,2,2,2,2,1,4,0,0,0,4,4,4,4,4,4,
+        1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+        1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+        1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+        1,0,0,0,0,0,0,0,0,4,4,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,1,
+        1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+        1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+        1,3,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,3,1,
+        1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+        1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+        1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1,
+        1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+        1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+        1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+    ]
+
+    const squares = []
+
+    //Legend
+    //0 - pac-dot 
+    //1 - wall
+    //2 - ghost lair
+    //3 - power-pellet
+    //4 - empty
+
+    //draw the grid and render it
+    function createBoard() {
+        for (let i=0; i < layout.length; i++) {
+            const square = document.createElement('div')
+            grid.appendChild(square)
+            squares.push(square)
+
+            //add layout to the board
+            if(layout[i] === 0) {
+                squares[i].classList.add('pac-dot')
+            } else if (layout[i] === 1) {
+                squares[i].classList.add('wall')
+            } else if (layout[i] === 3) {
+                squares[i].classList.add('power-pellet')
+            }
+        }
+    }
+    createBoard();
+
+ // starting position of pac-man
+ let pacmanCurrentIndex = 490
+
+ squares[pacmanCurrentIndex].classList.add('pac-man')
+
+ //move pac-man
+ function movePacman(e) {
+     squares[pacmanCurrentIndex].classList.add('pac-man')
+
+     switch(e.keyCode) {
+         case 37:
+             if(pacmanCurrentIndex % width !== 0) pacmanCurrentIndex -=1
+             break
+         case 38:
+             if(pacmanCurrentIndex % width !== 0) pacmanCurrentIndex -=1
+             break
+         case 39:
+             if(pacmanCurrentIndex % width !== 0) pacmanCurrentIndex -=1
+             break
+         case 40:
+             if(pacmanCurrentIndex % width !== 0) pacmanCurrentIndex -=1
+             break
+     }
+ }
+// video at 10:30
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
